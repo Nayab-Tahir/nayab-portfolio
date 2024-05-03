8dfd39aab16765754872f82374525d32964f0d23
Nayab-Tahir <nayabtahir321@gmail.com>
2024-05-04 02:53:17 +0500

    Email functionality added

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Button = (props) => {
   return (
-    <div className="button">
+    <div className="button" onClick={props.onClick}>
         <Link to={props.path} style={{fontSize: props.fontSize}} href="#">&#123;{props.text}&#125;</Link>
     </div>
   );
--- src/components/Sections/ContactSection/ContactSection.jsx
+++ src/components/Sections/ContactSection/ContactSection.jsx
@@ -2,8 +2,36 @@ import Section from "../../../UI/Section/Section";
 import SectionHead from "../../../UI/Section/SectionHead";
 import Button from "../../../UI/Button";
 import "./ContactSection.css";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
 
 const ContactSection = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = () => {
+    const params = { from_name: name, from_email: email, message: message };
+    if (email && name && message) {
+      console.log(email, name, message)
+      emailjs
+        .send("service_ytypvqa", "template_bf0odbo", params, {
+          publicKey: "i3otYtBH-jZDLIIfu",
+        })
+        .then(
+          () => {
+            console.log("SUCCESS!");
+            setName("");
+            setEmail("");
+            setMessage("");
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    }
+  };
+
   return (
     <Section styleClass="section-none">
       <SectionHead
@@ -15,10 +43,20 @@ const ContactSection = () => {
         <div className="form-container">
           <form className="contact-form" action="">
             <div>
-              <input type="text" placeholder="_name*" />
+              <input
+                type="text"
+                placeholder="_name*"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="_email*" />
+              <input
+                type="text"
+                placeholder="_email*"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div>
               <textarea
@@ -27,10 +65,12 @@ const ContactSection = () => {
                 cols="30"
                 rows="10"
                 placeholder="_message*"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
             </div>
             <div id="contact-btn-div">
-              <Button text="send" fontSize="18px" />
+              <Button text="send" fontSize="18px" onClick={handleSubmit} />
             </div>
           </form>
         </div>
